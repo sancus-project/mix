a41f127e045b96d91619c95a0266b7cabc99826d
Alejandro Mery <amery@jpi.io>
2021-04-19 09:32:07 +0000

    mix: Implement actual Mixer.MiddlewareHandler
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- mix/middleware.go
+++ mix/middleware.go
@@ -2,14 +2,68 @@ package mix
 
 import (
 	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
 
 	"go.sancus.dev/mix/types"
 )
 
 // Middleware
 func (m *Mixer) MiddlewareHandler(w http.ResponseWriter, r *http.Request, next http.Handler, prefix string) {
+	var err error
 
-	m.NotFound(w, r)
+	if prefix == "/" || prefix == "" {
+		// no prefix
+
+		if err = m.Handler(w, r); err == nil {
+			// Done.
+			return
+		}
+
+	} else {
+		// check prefix
+		var path string
+
+		rctx := chi.RouteContext(r.Context())
+		if rctx != nil && rctx.RoutePath != "" {
+			path = rctx.RoutePath
+		} else {
+			path = r.URL.Path
+		}
+
+		if s := strings.TrimPrefix(path, prefix); s != path {
+			if s == "" {
+				path = "/"
+			} else {
+				path = s
+			}
+
+			if path[0] == '/' {
+
+				// Update RoutePath before Handling
+				if rctx != nil {
+					rctx.RoutePath = path
+				} else {
+					r.URL.Path = path
+				}
+
+				if err = m.Handler(w, r); err == nil {
+					// Done.
+					return
+				}
+			}
+		}
+	}
+
+	// Failed
+	if err != nil {
+		m.config.ErrorHandler(w, r, err)
+	} else if next != nil {
+		next.ServeHTTP(w, r)
+	} else {
+		m.NotFound(w, r)
+	}
 }
 
 func (m *Mixer) Middleware(prefix string) types.MiddlewareHandlerFunc {
