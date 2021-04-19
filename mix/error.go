c2fc3733b9b5d1ca1f75a8f6c07f91736d4a9ffd
Alejandro Mery <amery@jpi.io>
2021-04-19 00:22:21 +0000

    mix: implement DefaultErrorHandler()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- mix/error.go
+++ mix/error.go
@@ -2,8 +2,29 @@ package mix
 
 import (
 	"net/http"
+
+	"go.sancus.dev/mix/types"
 )
 
 func DefaultErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
-	panic(err)
+
+	// does the error know how to render itself?
+	h, ok := err.(http.Handler)
+	if !ok {
+		var code int
+
+		// but if it doesn't, wrap it in HandlerError{}
+		if e, ok := err.(types.Error); ok {
+			code = e.Status()
+		} else {
+			code = http.StatusInternalServerError
+		}
+
+		h = &types.HandlerError{
+			Code: code,
+			Err:  err,
+		}
+	}
+
+	h.ServeHTTP(w, r)
 }
