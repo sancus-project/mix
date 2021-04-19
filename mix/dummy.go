779cdfbdfde06e330e912250106ac9fca41895e3
Alejandro Mery <amery@jpi.io>
2021-04-19 08:36:45 +0000

    mix: Add dummy Mixer.Close() and Mixer.Reload()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ mix/dummy.go
@@ -0,0 +1,11 @@
+package mix
+
+// Close
+func (m *Mixer) Close() error {
+	return nil
+}
+
+// Reload
+func (m *Mixer) Reload() error {
+	return nil
+}
--- types/mixer.go
+++ types/mixer.go
@@ -10,7 +10,11 @@ type Router interface {
 
 type Mixer interface {
 	Router
+
 	Mount(string, Router)
+
+	Close() error
+	Reload() error
 
 	Middleware(prefix string) MiddlewareHandlerFunc
 	Sitemap(prefix string) http.HandlerFunc
