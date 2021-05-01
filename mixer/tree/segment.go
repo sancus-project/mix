fdb0a26c8565e70d81b6d4a3041717dd89051fc4
Alejandro Mery <amery@jpi.io>
2021-05-01 14:19:09 +0000

    mixer/tree: Implement NewSegment() for "*" and literal strings
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- mixer/tree/segment.go
+++ mixer/tree/segment.go
@@ -1,10 +1,29 @@
 package tree
 
-type Segment interface{}
+type Segment interface {
+	Match(string) (Match, bool)
+}
+
+type Literal string
+
+func (v Literal) Match(s string) (Match, bool) {
+	if string(v) == s {
+		return s, true
+	}
+	return nil, false
+}
+
+type CatchAll struct{}
+
+func (_ CatchAll) Match(s string) (Match, bool) {
+	return s, true
+}
 
 func NewSegment(s string) (Segment, bool) {
-	if len(s) > 0 {
-		return s, true
+	if s == "*" {
+		return CatchAll{}, true
+	} else if len(s) > 0 {
+		return Literal(s), true
 	} else {
 		return nil, false
 	}
