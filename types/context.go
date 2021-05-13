6a138f958ffe4463b4cc32b0bab950fbcfc5ebe6
Alejandro Mery <amery@jpi.io>
2021-05-13 20:53:34 +0100

    Context: introduce Context.Next() to walk RoutePath
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- types/context.go
+++ types/context.go
@@ -29,7 +29,7 @@ func (rctx *Context) Init(ctx context.Context, prefix, path string) error {
 	} else if n := strings.IndexRune(path[1:], '/'); n < 0 {
 		pattern = filepath.Join(prefix, path)
 	} else {
-		pattern = filepath.Join(prefix, path[0:n+1]) + "/*"
+		pattern = filepath.Join(prefix, "*")
 	}
 
 	*rctx = Context{
@@ -39,4 +39,42 @@ func (rctx *Context) Init(ctx context.Context, prefix, path string) error {
 	}
 
 	return nil
+}
+
+func (rctx *Context) Next() (*Context, string) {
+
+	path := rctx.RoutePath
+
+	if len(path) > 1 {
+
+		s := path[1:]
+		prefix := rctx.RoutePrefix
+		pattern := strings.TrimSuffix("/*", rctx.RoutePattern)
+
+		if prefix == "/" {
+			prefix = ""
+		}
+
+		if n := strings.IndexRune(s, '/'); n < 0 {
+			prefix += path
+			pattern += path
+			path = ""
+
+		} else {
+			s = s[:n]
+			prefix += path[:n+1]
+			pattern += path[:n+1] + "/*"
+			path = path[n+1:]
+		}
+
+		next := &Context{
+			RoutePath: path,
+			RoutePrefix: prefix,
+			RoutePattern: pattern,
+		}
+
+		return next, s
+	}
+
+	return nil, ""
 }
