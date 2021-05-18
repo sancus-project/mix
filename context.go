9cc1ff8225795cbf5dd04e70acf2c129e97f7af2
Alejandro Mery <amery@jpi.io>
2021-05-18 01:05:32 +0100

    Context: moved to types/mctx
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- context.go
+++ context.go
@@ -3,54 +3,19 @@ package mix
 import (
 	"context"
 
-	"go.sancus.dev/mix/types"
+	"go.sancus.dev/mix/types/mctx"
 )
 
-type (
-	Context = types.Context
-)
+type Context = mctx.Context
 
-// RouteContext returns mix's routing Context object from a
-// http.Request Context.
-func RouteContext(ctx context.Context) *Context {
-	if rctx, ok := ctx.Value(RouteCtxKey).(*Context); ok {
-		return rctx
-	}
-	return nil
+func RouteContext(ctx context.Context) *mctx.Context {
+	return mctx.RouteContext(ctx)
 }
 
-// NewRouteContext returns a new routing Context object.
-func NewRouteContext(ctx context.Context, prefix, path string) *Context {
-	rctx := &Context{}
-	if err := rctx.Init(ctx, prefix, path); err != nil {
-		panic(err)
-	}
-	return rctx
+func WithRouteContext(ctx context.Context, rctx *mctx.Context) context.Context {
+	return mctx.WithRouteContext(ctx, rctx)
 }
 
-// WithRouteContext returns a new http.Request Context with
-// a given mix routing Context object connected to it, so it
-// can later be extracted using RouteContext()
-func WithRouteContext(ctx context.Context, rctx *Context) context.Context {
-	if ctx == nil {
-		ctx = context.Background()
-	}
-	if rctx == nil {
-		rctx = &Context{}
-	}
-	return context.WithValue(ctx, RouteCtxKey, rctx)
-}
-
-var (
-	// RouteCtxKey is the context.Context key to store the request context.
-	RouteCtxKey = &contextKey{"RouteContext"}
-)
-
-// contextKey is a value for use with context.WithValue
-type contextKey struct {
-	name string
-}
-
-func (k *contextKey) String() string {
-	return "mix context value " + k.name
+func NewRouteContext(ctx context.Context, prefix, path string) *mctx.Context {
+	return mctx.NewRouteContext(ctx, prefix, path)
 }
---
+++ types/mctx/context.go
@@ -0,0 +1,126 @@
+package mctx
+
+import (
+	"context"
+	"path/filepath"
+	"strings"
+)
+
+type Context struct {
+	RoutePrefix  string
+	RoutePath    string
+	RoutePattern string
+}
+
+// Clone() creates a copy of a routing Context object
+func (rctx Context) Clone() *Context {
+	return &rctx
+}
+
+func (rctx *Context) Init(ctx context.Context, prefix, path string) error {
+	var pattern string
+
+	if prefix == "" {
+		prefix = "/"
+	}
+
+	if path == "" {
+		pattern = prefix
+	} else if n := strings.IndexRune(path[1:], '/'); n < 0 {
+		pattern = filepath.Join(prefix, path)
+	} else {
+		pattern = filepath.Join(prefix, "*")
+	}
+
+	*rctx = Context{
+		RoutePrefix:  prefix,
+		RoutePattern: pattern,
+		RoutePath:    path,
+	}
+
+	return nil
+}
+
+func (rctx *Context) Next() (*Context, string) {
+
+	path := rctx.RoutePath
+
+	if len(path) > 1 {
+
+		s := path[1:]
+		prefix := rctx.RoutePrefix
+		pattern := strings.TrimSuffix("/*", rctx.RoutePattern)
+
+		if prefix == "/" {
+			prefix = ""
+		}
+
+		if n := strings.IndexRune(s, '/'); n < 0 {
+			prefix += path
+			pattern += path
+			path = ""
+
+		} else {
+			s = s[:n]
+			prefix += path[:n+1]
+			pattern += path[:n+1] + "/*"
+			path = path[n+1:]
+		}
+
+		next := &Context{
+			RoutePath:    path,
+			RoutePrefix:  prefix,
+			RoutePattern: pattern,
+		}
+
+		return next, s
+	}
+
+	return nil, ""
+}
+
+// RouteContext returns mix's routing Context object from a
+// http.Request Context.
+func RouteContext(ctx context.Context) *Context {
+
+	if rctx, ok := ctx.Value(RouteCtxKey).(*Context); ok {
+		return rctx
+	}
+	return nil
+}
+
+// NewRouteContext returns a new routing Context object.
+func NewRouteContext(ctx context.Context, prefix, path string) *Context {
+	rctx := &Context{}
+	if err := rctx.Init(ctx, prefix, path); err != nil {
+		panic(err)
+	}
+	return rctx
+}
+
+// WithRouteContext returns a new http.Request Context with
+// a given mix routing Context object connected to it, so it
+// can later be extracted using RouteContext()
+func WithRouteContext(ctx context.Context, rctx *Context) context.Context {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	if rctx == nil {
+		rctx = &Context{}
+	}
+	return context.WithValue(ctx, RouteCtxKey, rctx)
+}
+
+var (
+	// RouteCtxKey is the context.Context key to store the request context.
+	RouteCtxKey = &contextKey{"RouteContext"}
+)
+
+// contextKey is a value for use with context.WithValue
+type contextKey struct {
+	name string
+}
+
+func (k *contextKey) String() string {
+	return "mix context value " + k.name
+}
