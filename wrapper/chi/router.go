46e76c6b1d204937aa2b0f6e12d8bdbc35064d35
Alejandro Mery <amery@jpi.io>
2021-05-23 05:41:12 +0100

    wrapper/chi: implement initial TryServeHTTP() and PageInfo()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- wrapper/chi/router.go
+++ wrapper/chi/router.go
@@ -1,19 +1,44 @@
 package chi
 
 import (
+	"context"
 	"net/http"
 
-	"go.sancus.dev/web/errors"
+	"github.com/go-chi/chi/v5"
+
+	sancus "go.sancus.dev/web/context"
+	"go.sancus.dev/web/intercept"
 )
 
 type ChiWrapper struct {
 	h ChiHandler
 }
 
-func (h *ChiWrapper) TryServeHTTP(w http.ResponseWriter, r *http.Request) error {
-	return errors.ErrNotFound
+func (cw *ChiWrapper) TryServeHTTP(w http.ResponseWriter, r *http.Request) error {
+	cc := RouteContext(r)
+	ctx := WithRouteContext(r, cc)
+	h := intercept.Intercept(cw.h)
+
+	return h.TryServeHTTP(w, r.WithContext(ctx))
 }
 
-func (h *ChiWrapper) PageInfo(r *http.Request) (interface{}, bool) {
-	return nil, false
+func (cw *ChiWrapper) PageInfo(r *http.Request) (interface{}, bool) {
+
+	cc := RouteContext(r)
+	found := cw.h.Match(cc, r.Method, cc.RoutePath)
+
+	return nil, found
+}
+
+func RouteContext(r *http.Request) *chi.Context {
+	rctx := sancus.RouteContext(r.Context())
+
+	return &chi.Context{
+		RoutePath:   rctx.RoutePath,
+		RouteMethod: r.Method,
+	}
+}
+
+func WithRouteContext(r *http.Request, cc *chi.Context) context.Context {
+	return context.WithValue(r.Context(), chi.RouteCtxKey, cc)
 }
