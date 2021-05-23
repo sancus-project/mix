3d7d8ca593a85937fd1eaeee8b31c0d0028218fe
Alejandro Mery <amery@jpi.io>
2021-05-23 19:00:14 +0100

    wrapper/adhoc: use go.sancus.dev/web/intercept
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- wrapper/adhoc/router.go
+++ wrapper/adhoc/router.go
@@ -3,50 +3,25 @@ package adhoc
 import (
 	"net/http"
 
-	"go.sancus.dev/web/errors"
+	"go.sancus.dev/web/intercept"
 )
 
 type AdhocWrapper struct {
 	h http.Handler
 }
 
-func (h *AdhocWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.h.ServeHTTP(w, r)
-}
-
-func TryHandler(w0 http.ResponseWriter, r *http.Request, h http.Handler, err_out *error) {
-	var err error
-
-	defer func() {
-		if err = errors.Recover(); err != nil {
-			if err_out == nil {
-				panic(err)
-			} else {
-				*err_out = err
-			}
-		}
-	}()
-
-	w := Writer{w: w0}
-	h.ServeHTTP(&w, r)
-
-	if err = w.Error(); err == nil {
-		err = w.Flush()
-	}
-
-	if err_out != nil {
-		*err_out = err
-	} else if err != nil {
-		panic(errors.Panic(err))
-	}
-}
-
 func (h *AdhocWrapper) TryServeHTTP(w http.ResponseWriter, r *http.Request) error {
-	var err error
-	TryHandler(w, r, h.h, &err)
-	return err
+	f := intercept.Intercept(h.h)
+	return f.TryServeHTTP(w, r)
 }
 
 func (h *AdhocWrapper) PageInfo(r *http.Request) (interface{}, bool) {
-	return nil, false
+	w := &intercept.DummyWriter{}
+
+	h.h.ServeHTTP(w, r)
+	if err := w.Error(); err != nil {
+		return err, false
+	} else {
+		return nil, true
+	}
 }
